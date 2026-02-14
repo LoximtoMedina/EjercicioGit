111903667da7893e5a3bddfbf54cf586f4d35177
LoximtoMedina <crjosuem911@gmail.com>
2026-02-14 17:50:35 -0600

    Commit 4: Configuración inicial de Program y simulación de viajes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
 ﻿// Cristian Josué Medina Galeano - 20251900163
+using System;
+
+namespace EjercicioGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Datos
+            Console.WriteLine("Alumno: Cristian Josué Medina Galeano - 20251900163");
+
+            // Instancias
+            BusUrbano miBus = new BusUrbano("BUS-504", 40, 80);
+            Taxi miTaxi = new Taxi("TX-COM", 4, 100);
+            BicicletaElectrica miBici = new BicicletaElectrica("BICI-01", 1, 30);
+
+            Vehiculo[] flota = { miBus, miTaxi, miBici };
+
+            // Simulación de Viajes (10 Km y luego 30 Km)
+            foreach (var v in flota) v.RecorrerKilometros(10);
+            foreach (var v in flota) v.RecorrerKilometros(30);
+        }
+    }
+}
